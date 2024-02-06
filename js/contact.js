b37cf7cda3388183c732cac31cfe57c312aedbc3
Lightning 77 yt <villamnyul77@gmail.com>
2024-02-06 13:28:20 +0100

    Apróbb változtatások.

--- js/contact.js
+++ js/contact.js
@@ -2,5 +2,5 @@ let inputs = document.querySelectorAll("input"),
     btn = document.querySelector("button");
 
 btn.addEventListener("click", ()=>{
-    alert("Név: " + inputs[0].value + "\nÜzenet: \n" + inputs[1].value);
+    alert("Név: " + inputs[0].value + "\nÜzenet: " + inputs[1].value);
 });
